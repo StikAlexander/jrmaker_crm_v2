ba61ef03a4282b0b363f1dbd143ffff0ed464872
StikAlexander <161220374+StikAlexander@users.noreply.github.com>
2024-09-11 13:08:24 -0500

    se revirtio el commit de (error de rutas relacionado con login default de laravel solucionado) debido a que dañaba la confirmacion de email a crear usuarios

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +25,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+         // Configuración para habilitar la verificación de correo electrónico
+        Auth::routes(['verify' => true]);
+
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
