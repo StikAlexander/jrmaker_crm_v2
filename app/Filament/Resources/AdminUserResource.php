1b2831eb81631edf5ff9a43b73033145e794851d
StikAlexander <161220374+StikAlexander@users.noreply.github.com>
2024-09-24 17:18:40 -0500

    probando cambios webhook port x2

--- app/Filament/Resources/AdminUserResource.php
+++ app/Filament/Resources/AdminUserResource.php
@@ -42,7 +42,7 @@ class AdminUserResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-user';
     protected static ?string $navigationGroup = 'Usuarios';
     protected static ?int $navigationSort = 3;
-    protected static ?string $pluralLabel = 'probando cambios webhook';
+    protected static ?string $pluralLabel = 'probando cambios';
     protected static ?string $singularLabel = 'Administrador';
 
     public static function form(Form $form): Form
