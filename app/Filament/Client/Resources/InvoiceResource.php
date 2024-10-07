568f534d178e2f692e0937e04313c8305aeb5fe6
StikAlexander <161220374+StikAlexander@users.noreply.github.com>
2024-10-06 22:39:57 -0500

    factura pendiente

--- app/Filament/Client/Resources/InvoiceResource.php
+++ app/Filament/Client/Resources/InvoiceResource.php
@@ -20,10 +20,9 @@ class InvoiceResource extends Resource
 {
     protected static ?string $model = Invoice::class;
     protected static ?string $recordTitleAttribute = 'invoice_number';
-    protected static ?string $pluralLabel = 'Facturas';
-    protected static ?string $singularLabel = 'Factura';
+    protected static ?string $pluralLabel = 'Facturas Pendientes';
+    protected static ?string $singularLabel = 'Factura Pendiente';
     protected static ?string $navigationIcon = 'heroicon-o-banknotes';
-    protected static ?string $navigationGroup = 'Contabilidad';
     protected static ?int $navigationSort = 1;
 
     // Filtrar facturas del cliente autenticado
