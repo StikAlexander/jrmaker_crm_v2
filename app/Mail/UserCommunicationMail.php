d7d9273caba38837bffe69a53b2b392d5d964136
StikAlexander <161220374+StikAlexander@users.noreply.github.com>
2024-10-14 11:11:07 -0500

    .

--- app/Mail/UserCommunicationMail.php
+++ app/Mail/UserCommunicationMail.php
@@ -3,78 +3,52 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;  
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class UserCommunicationMail extends Mailable implements ShouldQueue  
+class UserCommunicationMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
     public $cliente;
     public $plantilla;
 
-    /**
-     * Create a new message instance.
-     *
-     * @param $cliente
-     * @param $plantilla
-     */
     public function __construct($cliente, $plantilla)
     {
         $this->cliente = $cliente;
         $this->plantilla = $plantilla;
     }
 
-    /**
-     * Get the message envelope.
-     *
-     * @return Envelope
-     */
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'User Communication: ' . $this->getSubject(),
+            subject: '' . $this->getSubject(),
         );
     }
 
-    /**
-     * Get the message content definition.
-     *
-     * @return Content
-     */
     public function content(): Content
     {
         return new Content(
-            view: $this->getView(),
-            with: ['cliente' => $this->cliente],
+            view: $this->getView(),  
+            with: ['cliente' => $this->cliente]  
         );
     }
 
-    /**
-     * Determine which view to use based on the selected template.
-     *
-     * @return string
-     */
     protected function getView(): string
     {
         switch ($this->plantilla) {
             case 'solicitud_certificados':
-                return 'emails.solicitud_certificados';  // Vista Blade para solicitud de certificados
+                return 'emails.solicitud_certificados';  
             case 'publicidad':
-                return 'emails.publicidad';  // Vista Blade para correo publicitario
+                return 'emails.publicidad';
             default:
-                return 'emails.default';  // Vista predeterminada si no se selecciona plantilla
+                return 'emails.default';
         }
     }
 
-    /**
-     * Get the subject based on the template.
-     *
-     * @return string
-     */
     protected function getSubject(): string
     {
         switch ($this->plantilla) {
@@ -87,11 +61,6 @@ class UserCommunicationMail extends Mailable implements ShouldQueue
         }
     }
 
-    /**
-     * Attachments for the email.
-     *
-     * @return array
-     */
     public function attachments(): array
     {
         return [];
