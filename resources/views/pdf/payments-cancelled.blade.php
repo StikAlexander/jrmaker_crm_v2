5384ba26b6026f676bad7f4032aef8f7f4d303ee
StikAlexander <161220374+StikAlexander@users.noreply.github.com>
2024-10-19 19:41:21 -0500

    logo en reporte

--- resources/views/pdf/payments-cancelled.blade.php
+++ resources/views/pdf/payments-cancelled.blade.php
@@ -23,10 +23,26 @@
         .text-right {
             text-align: right;
         }
+        .logo {
+            width: 150px;
+            height: auto;
+        }
     </style>
     <title>Pagos Cancelados</title>
 </head>
 <body>
+
+    <table>
+        <tr>
+            <td><img src="{{ url('storage/images/jr_maker_logo.png') }}" alt="Logo JR Maker" class="logo"></td>
+            <td class="text-right">
+                JR Maker SAS <br>
+                Dirección de la empresa <br>
+                Ciudad, País
+            </td>
+        </tr>
+    </table>
+
     <h2 class="text-center">Reporte de Pagos Cancelados</h2>
     <table>
         <thead>
